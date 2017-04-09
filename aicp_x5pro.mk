61dd637e92edda8312a6489ea48d016eaf6b83fc
Yuri <SeriniTY320@gmail.com>
2017-04-09 23:00:45 +0300

    Update aicp_x5pro.mk

--- aicp_x5pro.mk
+++ aicp_x5pro.mk
@@ -11,7 +11,7 @@ $(call inherit-product, device/doogee/x5pro/full_x5pro.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := x5pro
-PRODUCT_NAME := aosp_x5pro
+PRODUCT_NAME := aicp_x5pro
 PRODUCT_BRAND := Doogee
 PRODUCT_MODEL := Doogee X5PRO
 PRODUCT_MANUFACTURER := Doogee
