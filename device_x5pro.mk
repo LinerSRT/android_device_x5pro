94bc72621226b13b7bc2b8bbf27158ac1ebb4dc9
Yuri <SeriniTY320@gmail.com>
2017-04-14 23:15:38 +0300

    add OTA

--- device_x5pro.mk
+++ device_x5pro.mk
@@ -127,6 +127,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/system/etc/permissions/android.hardware.microphone.xml:system/etc/permissions/android.hardware.microphone.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml 
 
+ADDITIONAL_BUILD_PROPERTIES += \
+cm.updater.type=plain \
+cm.updater.uri=https://raw.githubusercontent.com/SeriniTY320/updater/master/update.json \
+
 # USB
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp,adb
