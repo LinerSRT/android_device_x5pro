96b5aa1f776c429982ce009688476f898ffd14ea
SeriniTY320 <SeriniTY320@gmail.com>
2017-04-16 09:42:52 +0300

    switch to AOKP

---
+++ aokp.mk
@@ -0,0 +1,17 @@
+## Specify phone tech before including full_phone
+
+# Release name
+PRODUCT_RELEASE_NAME := x5pro
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/doogee/x5pro/device_x5pro.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := x5pro
+PRODUCT_NAME := aokp_x5pro
+PRODUCT_BRAND := Doogee
+PRODUCT_MODEL := Doogee X5PRO
+PRODUCT_MANUFACTURER := Doogee
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 # Use this to add x5pro to CM's lunch command menu
 for var in eng user userdebug; do
-  add_lunch_combo aosp_x5pro-$var
+  add_lunch_combo aokp_x5pro-$var
 done
